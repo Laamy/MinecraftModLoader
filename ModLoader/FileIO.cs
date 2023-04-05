1ff9f7c122cc46e43cc83ed837bd57b9f4b606e3
yeemi routh <Shade4Keal@gmail.com>
2023-04-06 01:36:47 +1000

    file io

---
+++ ModLoader/FileIO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace ModLoader
+{
+    internal class FileIO
+    {
+        public static DirectoryInfo ConfigFolder { get; private set; }
+
+        public static void Initialize()
+        {
+            ConfigFolder = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\RoamingState\\MML");
+
+            if (!Directory.Exists(ConfigFolder.FullName))
+            {
+                Directory.CreateDirectory(ConfigFolder.FullName);
+            }
+        }
+
+        public class Folders
+        {
+            public static bool CheckCreate(string path)
+            {
+                if (!Directory.Exists(ConfigFolder.FullName + "\\" + path))
+                {
+                    Directory.CreateDirectory(ConfigFolder.FullName + "\\" + path);
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public class Files
+        {
+            public static bool CheckCreate(string path)
+            {
+                if (!File.Exists(ConfigFolder.FullName + "\\" + path))
+                {
+                    File.Create(ConfigFolder.FullName + "\\" + path);
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
+}
--- ModLoader/Program.cs
+++ ModLoader/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ModLoader
+﻿using System;
+
+namespace ModLoader
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            FileIO.Initialize();
 
+            Console.WriteLine(FileIO.ConfigFolder.FullName);
+
+            if (FileIO.Folders.CheckCreate("mods"))
+            {
+
+            }
+            else
+            {
+                // inject mods
+            }
         }
     }
 }
